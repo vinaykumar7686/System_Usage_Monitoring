228626f03724da08842192292a5b2ad4c967c1ac
Vinay <vinaykumarvk.8866@gmail.com>
2020-06-09 20:48:52 +0530

    Current Python Script is able to export the name of current application, launch time, date and day into a csv file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from ctypes import windll, create_unicode_buffer
-import time
+import pandas as pd
+import time,os,sys,datetime
+
 def get_Active_Window_Title():
+    '''
+    Returns currently active applications' title
+    '''
     acw=windll.user32.GetForegroundWindow()
     length=windll.user32.GetWindowTextLengthW(acw)
     buf = create_unicode_buffer(length+1)
@@ -9,11 +14,32 @@ def get_Active_Window_Title():
         return buf.value
     else:
         return None
+
 if __name__ == "__main__":
+    #Initialising Previous application Variable as None
     prev=None
+
+    #Running an Infinite Loop 
     while True:
+        #Getting active applications' title
         aw=get_Active_Window_Title()
+
+        #True iff previos and current apps are different(To avoid repeated entries)
         if prev!=aw and not aw==None:
+
             print(aw)
+            
+            #creating a dictionary that stores the data to be pushed into dataset 
+            data={'application':[aw],'stime':[(datetime.datetime.now()).strftime("%X")],'day':[(datetime.datetime.now()).strftime("%A")],
+            'date':[(datetime.datetime.now()).strftime("%x")]}
+            
+            #converting dictionary into dataframe
+            df=pd.DataFrame(data, columns=['application', 'stime', 'day', 'date'])
+
+            #appending dataframe to external csv file
+            df.to_csv('usagelog.csv', mode='a',header= False,index=False)
+
+            #updating prev to current applications' name
             prev=aw
+    
 
